12e8c8135a165ecfc1dead13b2f121c61c93db46
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2025-05-27 18:33:13 -0300

    corpo da requisição em JSON (Stream e Buffer)

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -3,18 +3,29 @@ import http from "node:http";
 const users = [];
 
 const PORT = 3000;
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  const buffers = [];
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
 
   if (method === "GET" && url === "/users") {
     res.setHeader("Content-Type", "application/json");
     return res.end(JSON.stringify(users));
   }
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
     users.push({
       id: 1,
-      name: "Julio Paschoal",
-      email: "juliocpaschoal@gmail.com",
+      name,
+      email,
     });
     return res.end("User created");
   }
